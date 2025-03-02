6e3f32480967912d0fa14647ddf186bc096feef0
pyl0ader <38451269+pyl0ader@users.noreply.github.com>
2025-03-02 16:24:48 -0600

    Add files via upload
    still getting a "not enough arguments" error:(

--- packer.scm
+++ packer.scm
@@ -1,17 +1,18 @@
-(define (pack-sprites
+(define (pack-sprites image
 (let* (
+    (spriteWidth 750)
+    (spriteHeight 1026)
     (cols 5)
-    (sprite_width 750)
-    (sprite_height 1026)
-    (image (car(vector->list(cadr(gimp-image-list)))))
     (layers (vector->list(cadr(gimp-image-get-layers image))))
+    (layersCount (car(gimp-image-get-layers image)))
     )
+    (gimp-message "A")
     (let loop ((i 0 ))
-    (if (< i 14))
+    (if (< i layersCount))
     (begin
     (let* (
-        (offx (* sprite_width (modulo i cols)))
-        (offy (* sprite_height (floor (/ i cols))))
+        (offx (* spriteWidth (modulo i cols)))
+        (offy (* spriteHeight (floor (/ i cols))))
         (layer (car layers))
         (set! layers (cdr layers))
         )
@@ -24,7 +25,8 @@
     "<Image>/Tools/pack sprites"
     "pack layers into spritesheet"
     "plugnpi"
-    "plugnpi"
+    ""
     "March 2025"
-    "RGB*, GRAY*"
+    ""
+    SF-IMAGE    "image" 0
 )
